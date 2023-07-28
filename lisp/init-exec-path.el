78362341ae04ce747f83bd2ede34e47b3b47b418
ts <sunting.bcwl@gmail.com>
2023-07-28 23:19:46 +0800

    Add youdao secret key.

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -5,7 +5,8 @@
 (use-package exec-path-from-shell
   :demand t
   :config
-  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "NIX_SSL_CERT_FILE" "NIX_PATH"))
+  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"
+                 "NIX_SSL_CERT_FILE" "NIX_PATH" "YOUDAO_APP_KEY" "YOUDAO_SECRET_KEY"))
     (add-to-list 'exec-path-from-shell-variables var))
   (when (or (memq window-system '(mac ns x pgtk))
           (unless (memq system-type '(ms-dos windows-nt))
