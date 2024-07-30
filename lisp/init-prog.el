ed6fe6c3fd2359ab20c9f1fce8b73dffb36b4376
ts <sunting.bcwl@gmail.com>
2024-07-31 07:53:16 +0800

    Add kotlin mode.

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -189,6 +189,7 @@ Install the doc if it's not installed."
 (use-package solidity-mode)
 (use-package groovy-mode)
 (use-package bazel)
+(use-package kotlin-ts-mode)
 
 (use-package protobuf-mode
   :hook (protobuf-mode . (lambda ()
